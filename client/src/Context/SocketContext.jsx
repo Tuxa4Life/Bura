815c0ac3faab9087c5c4cd4536b95c3e291cc6d1
Tuxa4Life <nikoloztuxa@gmail.com>
2025-08-05 18:33:34 +0400

    Update: server url

--- client/src/Context/SocketContext.jsx
+++ client/src/Context/SocketContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:5000')
+const socket = io(process.env.REACT_APP_SERVER_URL || 'http://localhost:5000')
 
 const SocketContext = createContext()
 const SocketProvider = ({ children }) => {
